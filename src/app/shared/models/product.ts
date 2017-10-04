80dcd69dd4aeb2302a8a6c6fcb7c281cf0d08d9b
hkb <hkb@tisynch.com>
2017-10-04 11:14:33 +0200

    added product model

---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by hkb on 04.10.17.
+ */
+export interface IProduct {
+  id: string;
+  productName: string;
+  productCode: string;
+  releaseDate: Date;
+  lastModified: Date;
+  price: number;
+  description: string;
+  starRating: number;
+  imageUrl: string;
+}
