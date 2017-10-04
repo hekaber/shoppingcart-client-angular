b179f8a81337eb833fc4e2c438b6887a2afc8854
hkb <hkb@tisynch.com>
2017-10-04 17:03:46 +0200

    created product list component

---
+++ src/app/modules/home/product-list/product-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../../../shared/models/product';
+import { ProductService } from '../../../shared/providers/product.service';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  public toggleText: string  = 'Hide Images';
+  public products: Observable<IProduct[]>;
+  private _displayImg: boolean = true;
+
+  listFilter = '';
+
+  constructor(private _productService: ProductService) {}
+
+  ngOnInit() {
+    console.log('Product list Init!!!!!!');
+    this.products = this._productService.getProducts();
+  }
+
+
+  toggleImage(): void {
+    this._displayImg = !this._displayImg;
+    this.toggleText = this._displayImg ? 'Hide Images' : 'Display Images';
+  }
+
+}
