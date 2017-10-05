e8fbb8680a28dd495c8ae754ae534743fef2e037
hkb <hkb@tisynch.com>
2017-10-05 14:10:44 +0200

    refactored name in test

--- src/app/shared/resolves/product-resolve.spec.ts
+++ src/app/shared/resolves/product-resolve.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { ProductResolveService } from './product-resolve';
+import { ProductDetailResolve } from './product-resolve';
 
 describe('ProductResolveService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProductResolveService]
+      providers: [ProductDetailResolve]
     });
   });
 
-  it('should be created', inject([ProductResolveService], (service: ProductResolveService) => {
+  it('should be created', inject([ProductDetailResolve], (service: ProductDetailResolve) => {
     expect(service).toBeTruthy();
   }));
 });
