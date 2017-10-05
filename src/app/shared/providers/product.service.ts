1b0545223d4b4f6f229f12fedfe9a5ce9ddc52d4
hkb <hkb@tisynch.com>
2017-10-05 17:15:05 +0200

    last fix before refactoring attempt

--- src/app/shared/providers/product.service.ts
+++ src/app/shared/providers/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http, RequestOptions, Headers, Response} from '@angular/http';
+import {Http, RequestOptions, Headers, Response, RequestMethod, RequestOptionsArgs} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
@@ -10,16 +10,24 @@ import 'rxjs/add/operator/mergeMap';
 import { IProduct } from '../models/product';
 import { EndpointsProvider } from './endpoints';
 import { AUTH_TOKEN, AUTHORIZATION_HEADER } from "../constants";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
 
 @Injectable()
 export class ProductService {
+
+  // It's our readonly observable to be aware of changes on our product collection
+  readonly products$: Observable<Array<IProduct>>;
+  // It's our BehaviorSubject, which will emit new version of our collection
+  private _products$: BehaviorSubject<Array<IProduct>>;
+  // It's our dataStore: our in-memory products collection
+  private dataStore: IProductServiceStore;
 
   constructor(
     private readonly _http: Http,
     private readonly _endpoints: EndpointsProvider
   ) { }
 
-  getProducts(): Observable<Array<IProduct>> {
+  getAll(): Observable<Array<IProduct>> {
     // TODO make a request wrapper
     let token = localStorage.getItem(AUTH_TOKEN);
     let headers: Headers = new Headers({'Authorization': token});
@@ -46,8 +54,31 @@ export class ProductService {
       .catch(this._handleError('Product was not found'));
   }
 
+  update(id: string, product: IProduct): Observable<IProduct> {
+    // TODO make a request wrapper
+    let token = localStorage.getItem(AUTH_TOKEN);
+    let headers: Headers = new Headers({'Authorization': token});
+    let options: RequestOptions = new RequestOptions({headers: headers});
+
+
+    return this._http.put(this._endpoints.getProduct(id), JSON.stringify(product), options)
+      .do((res: Response) => console.log(`PUT query to '${res.url}':'${res.status}'`))
+      .map(res => res.json())
+      .catch(this._handleError('Product was not found'));
+  }
+
   save(product: IProduct): Observable<IProduct> {
     return null;
+  }
+
+  private _request<T>(method: RequestMethod, url: string, payload?: IProduct): Observable<T> {
+    const requestOption: RequestOptionsArgs = {             // We build the request options: method, payload
+      method: method,
+      body: payload ? payload : null
+    }
+    return this._http.request(url, requestOption)    // Call the Http service get method with the given URL
+      .map(res => res.json())                               // Transform Http Response into a JSON Object
+      // .do(product => this._syncDataStore(product))          // Sync server response with dataStore
   }
 
   private _handleError(errorMessage: string) {
@@ -56,4 +87,8 @@ export class ProductService {
       return Observable.throw(new Error(error));
     };
   }
+}
+
+export interface IProductServiceStore {
+  products: Array<IProduct>;
 }
