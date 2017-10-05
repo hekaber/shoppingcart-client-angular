7d29ac8c977d42e287a4c3f1a67266da7a6d591c
hkb <hkb@tisynch.com>
2017-10-05 13:14:20 +0200

    created file for constants

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by hkb on 05.10.17.
+ */
+export const AUTH_TOKEN: string = 'auth_token';
+export const AUTHORIZATION_HEADER: string = 'Authorization';
+export const CONTENT_TYPE: string = 'Content-Type';
+export const APP_JSON: string = 'application/json';
